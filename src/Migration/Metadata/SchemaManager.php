46e65ef7286be675b4d62926d2569140b3e70dd8
Ben Challis <ben-challis@users.noreply.github.com>
2023-01-02 17:58:01 +0000

    Document metadata SchemaManager (#26)

--- src/Migration/Metadata/SchemaManager.php
+++ src/Migration/Metadata/SchemaManager.php
@@ -9,17 +9,23 @@ use Amp\Sql\Result;
 use Amp\Sql\Statement;
 
 /**
+ * Manages the schema of the migration metadata within a persistence context.
+ *
  * @template TResult of Result
  * @template TStatement of Statement
  */
 interface SchemaManager
 {
     /**
+     * Tests if the metadata schema is currently up-to-date and ready to use.
+     *
      * @param Executor<TResult, TStatement> $executor
      */
     public function isUpToDate(Executor $executor): bool;
 
     /**
+     * Updates the metadata schema (or does nothing if not required) to the latest version.
+     *
      * @param Executor<TResult, TStatement> $executor
      */
     public function updateIfRequired(Executor $executor): void;
