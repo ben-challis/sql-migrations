47ad392b8f20c1e8a881af34f29559b6b9cec62b
Ben Challis <ben-challis@users.noreply.github.com>
2023-01-02 16:48:30 +0000

    Document Version and MetadataStore (#18)
    * `MetadataStore` usually deals with persistence.
    * `MetadataStore::fetchAll()` keys on version.
    * `Version` is assumed to be ascending in order, but not sequential.

--- src/Migration/Metadata/MetadataStore.php
+++ src/Migration/Metadata/MetadataStore.php
@@ -7,17 +7,27 @@ namespace BenChallis\SqlMigrations\Migration\Metadata;
 use BenChallis\SqlMigrations\Migration\Version;
 use Psl\Collection\MapInterface;
 
+/**
+ * A store of migration metadata, usually persisted.
+ */
 interface MetadataStore
 {
+    /**
+     * Saves a migration's metadata.
+     */
     public function save(Metadata $metadata): void;
 
     /**
-     * @throws MetadataDoesNotExist
+     * Fetches a migration's metadata by version.
+     *
+     * @throws MetadataDoesNotExist If the metadata does not exist in the store.
      */
     public function fetch(Version $version): Metadata;
 
     /**
-     * @return MapInterface<int, Metadata>
+     * Fetches all migration metadata from the store.
+     *
+     * @return MapInterface<int, Metadata> A map of metadata keyed by version.
      */
     public function fetchAll(): MapInterface;
 }
